b35c5b251f771f3ca8f5a53ec2ffa30092c40470
Jason Beetham <beefers331@gmail.com>
2023-12-22 23:16:08 -0700

    Added rust example

--- config.nims
+++ config.nims
@@ -7,5 +7,9 @@ task installLib, "installs libnimib":
   exec("sudo mv src/libnimib.so /usr/lib64/")
 
 task testc, "builds the C program":
-  exec("gcc -Iinclude -lnimib -o test.out tests/test.c")
-  exec("./test.out")
+  exec("gcc -Iinclude -lnimib -o test.c.out tests/test.c")
+  exec("./test.c.out")
+
+task testrust, "builds the Rust program":
+  exec("rustc  -o test.rust.out tests/test.rs")
+  exec("./test.rust.out")
---
+++ tests/test.rs
@@ -0,0 +1,46 @@
+use std::file;
+use std::ffi::c_char;
+use std::ffi::CString as CString;
+
+type RealCstring = *const c_char;
+
+#[link(name = "nimib")]
+extern {
+    fn nimib_init(file_path: RealCstring) -> RealCstring;
+    fn nimib_add_block(name: RealCstring, source: RealCstring, output: RealCstring);
+    fn nimib_add_code(code: RealCstring) -> RealCstring;
+    fn nimib_add_text(text: RealCstring);
+    fn nimib_save();
+    static mut nimib_exec_cmd: RealCstring;
+    static mut nimib_file_ext: RealCstring;
+
+}
+
+use std::stringify;
+fn main(){
+    unsafe{
+        let file_ext = CString::new(".rs").expect("Cannot Allocate");
+        let exec_cmd = CString::new("rustc -o $file.out $file >> /dev/null; $file.out").expect("Cannot Allocate");
+
+        nimib_file_ext = file_ext.as_ptr();
+        nimib_exec_cmd = exec_cmd.as_ptr();
+
+        let file_name = CString::new(file!()).expect("Cannot Allocate");
+        nimib_init(file_name.as_ptr());
+        let title_text = CString::new(r#"# This is Nimib from Rust!
+
+This is sorta cool, though it's Rust so .... bleh"#).expect("Cannot Allocate");
+        nimib_add_text(title_text.as_ptr());
+
+        let program = CString::new(stringify!( // This really does not multiline?!
+            fn main(){
+                println!("Hello, world!");
+                println!("Hmm!");
+            })).expect("Cannot Allocate");
+ 
+        nimib_add_code(program.as_ptr());
+        let subtext = CString::new("It's pretty easy still").expect("Cannot Allocate");
+        nimib_add_text(subtext.as_ptr());
+        nimib_save();
+    }
+}
